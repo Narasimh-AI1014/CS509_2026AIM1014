a51ed95dafd1a56ba3fe8ed118ed492c9c85b827
Muppuri Malyadri Lakshmi Narasimha <2026aim1014@iitrpr.ac.in>
2026-08-02 11:04:50 +0530

    The first commit pusing matrix mutliplication.

---
+++ assignment_01/driver/matrix_multiplication.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+vector<vector<int>> matMul(const vector<vector<int>> &mat1,const vector<vector<int>> &mat2,int M,int K,int N){
+    vector<vector<int>> result(M,vector<int>(N,0));
+    
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            for(int k=0;k<K;k++){
+                result[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+        }
+    }
+    return result;
+}
---
+++ common_wrapper/wrapper.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <chrono>
+
+#include <filesystem>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+
+    while(true){
+        cout<<"=================================";cout<<endl;
+        cout<<"||  1. Assignment_01           ||";cout<<endl;
+        cout<<"||  2. Exit                    ||";cout<<endl;
+        cout<<"=================================";cout<<endl;
+        cout<<endl;
+        cout<<endl;
+
+
+
+
+
+        int ass_no;
+        cout<<"choose assignment : ";
+        cin>>ass_no;
+        cout<<endl;
+
+        if(ass_no==1){
+            cout<<endl<<endl;
+            cout<<"<<=============================================>>";cout<<endl;
+            cout<<"=================================================";cout<<endl;
+            cout<<"||  1. Matrix Multiplication simple loops      ||";cout<<endl;
+            cout<<"||  2. Tilling Method                          ||";cout<<endl;
+            cout<<"||  3. Compare Time complexity b/w two methods ||";cout<<endl;
+            cout<<"=================================================";cout<<endl;
+
+            int choice;
+            cout<<"Choose Option : ";
+            cin>>choice;
+            cout<<endl;
+            
+
+            switch(choice){
+
+                case 1: {
+                        // std::string test = "C:/Users/muppu/OneDrive/Desktop/CS509/Individual/assignment_01/tests/test_01.txt";
+                        std::string execute = "C:/Users/muppu/OneDrive/Desktop/CS509/Individual/assignment_01/assignment_01_exectue_01";
+
+                        std::string drive = "C:/Users/muppu/OneDrive/Desktop/CS509/Individual/assignment_01/driver/matrix_multiplication.cpp";
+                        std::string src = "C:/Users/muppu/OneDrive/Desktop/CS509/Individual/assignment_01/src/matrix_multiplication.cpp";
+                        std::string compiler = "g++ "+drive+" "+src+" -o "+execute;
+                       
+
+                        std::string test_folder = "C:/Users/muppu/OneDrive/Desktop/CS509/Individual/assignment_01/tests";
+
+                        
+                        std::cout<<"compiling program....";
+                        system(compiler.c_str());
+
+                        // start storing the start time
+                        
+                        int i=1;
+                        for(const auto& entry: std::filesystem::directory_iterator(test_folder)){
+                            if(entry.is_regular_file()){
+
+                                
+                                std::string run = execute +" < "+ entry.path().string();
+                                system(run.c_str()); 
+                                i+=1;
+                            }
+                            
+                        }  
+                        
+                        
+                        break;
+                }
+                default:{
+                        cout<<"Unknow Operation";
+                        break;
+                }
+
+            }
+        }
+        if(ass_no==2){
+            cout<<"Exiting from program.";
+            cout<<endl;
+            break;
+        }
+}
+    return 0;
+}
